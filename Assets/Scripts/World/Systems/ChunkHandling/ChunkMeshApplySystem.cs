c5deaacb9ae9118a5df7e8b79c6efe4a451bac2f
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2019-10-03 22:24:25 +0300

    spread mesh apply over several frames

--- Assets/Scripts/World/Systems/ChunkHandling/ChunkMeshApplySystem.cs
+++ Assets/Scripts/World/Systems/ChunkHandling/ChunkMeshApplySystem.cs
@@ -1,4 +1,6 @@
-﻿using Unity.Entities;
+﻿using System.Diagnostics;
+using Unity.Entities;
+using UnityEngine;
 using World.Components;
 using World.Systems.Regions;
 
@@ -7,20 +9,26 @@ namespace World.Systems.ChunkHandling
     public class ChunkMeshApplySystem : ComponentSystem
     {
         private RegionLoadUnloadSystem _chunkCreationSystem;
+        private Stopwatch              _watch;
 
         protected override void OnCreate()
         {
             _chunkCreationSystem = World.GetOrCreateSystem<RegionLoadUnloadSystem>();
+            _watch               = new Stopwatch();
         }
 
         protected override void OnUpdate()
         {
+            long milisBudget = (long) (Time.fixedDeltaTime * 1000f);
+            _watch.Restart();
             var dict = _chunkCreationSystem.PosToChunk;
             Entities.WithAll<ChunkNeedMeshApply>().ForEach((Entity ent, ref ChunkPosComponent pos) =>
             {
+                if (_watch.ElapsedMilliseconds >= milisBudget) return;
                 dict[pos.Pos].ApplyMeshData();
                 PostUpdateCommands.RemoveComponent<ChunkNeedMeshApply>(ent);
             });
+            _watch.Stop();
         }
     }
 }
